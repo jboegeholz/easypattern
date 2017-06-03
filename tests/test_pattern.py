e8cbd2155f226134b89c7513205414b8dca2886a
jboegeholz <boegeholz.joern@gmail.com>
2017-06-03 15:37:06 +0200

    add travis config

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -1,13 +1,17 @@
+import unittest
 import re
 from easy_pattern import *
 
 
-regex = RegEx()
-regex.starts_with(zero_or_more(DIGIT)).followed_by(one_or_more(ALPHANUM)).followed_by(at_least(3, WHITESPACE))
+class PatternTest(unittest.TestCase):
 
-print(regex)
-p = re.compile(regex.regex)
-print p.findall('12345abc   ')
-assert p.findall('12345abc   ') == ['12345abc   ']
+    def test_pattern(self):
+        regex = RegEx()
+        regex.starts_with(zero_or_more(DIGIT)).followed_by(one_or_more(ALPHANUM)).followed_by(at_least(3, WHITESPACE))
 
-print('All tests passed')
+        print(regex)
+        p = re.compile(regex.regex)
+        print p.findall('12345abc   ')
+        assert p.findall('12345abc   ') == ['12345abc   ']
+
+        print('All tests passed')
