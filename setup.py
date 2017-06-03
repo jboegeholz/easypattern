9bfde2c4470aa40632ca98f301b283f0f6e79ccf
jboegeholz <boegeholz.joern@gmail.com>
2017-06-03 15:15:17 +0200

    add download_url

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=['easy_pattern'],
     url='https://github.com/jboegeholz/easypattern',
+    download_url='https://github.com/jboegeholz/easypattern/archive/0.1.tar.gz',
     license='MIT',
     author='Joern Boegeholz',
     author_email='boegeholz.joern@gmail.com',
